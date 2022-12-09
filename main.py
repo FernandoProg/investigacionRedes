7af05c2a3815bf8fce852427ce3e9d0a38defc1b
Fernando Cabezas <51241715+FernandoProg@users.noreply.github.com>
2022-12-09 11:03:03 -0300

    array de matrices sin terminar

--- main.py
+++ main.py
@@ -4,24 +4,33 @@ import sys
 
 start = time.time()
 
-if len(sys.argv) == 7:
+if len(sys.argv) == 5:  # py.exe .\main.py 1 0.9 0.1 1000
     seed = int(sys.argv[1])
-    tamaño_tabl = int(sys.argv[2])
-    
-    tamaño_pobl = int(sys.argv[3])
-    prob_cruza = float(sys.argv[4])
-    prob_mut = float(sys.argv[5])
-    num_ite = int(sys.argv[6])
+    prob_cruza = float(sys.argv[2])
+    prob_mut = float(sys.argv[3])
+    num_ite = int(sys.argv[4])
     print("semilla: ", seed)
-    print("tamaño_tablero: ", tamaño_tabl)
-    print("tamaño_población: ", tamaño_pobl)
     print("probabilidad_cruza: ", prob_cruza)
     print("probabilidad_mutación: ", prob_mut)
     print("número_iteraciones: ", num_ite)
 else:
     print('Error en la entrada de los parametros')
-    print('Los paramentros a ingresar son: semilla TamañoTablero TamañoPoblación ProbabilidadCruza ProbabilidadMutación NumeroIteración')
     sys.exit(0)
+
+np.random.seed(seed)
+
+typeVNF = 3
+redundantVNF = 4
+confiability = 0.99
+
+population = np.array(np.random.randint(2, size = (typeVNF, redundantVNF)))
+print(population.shape)
+while population.shape[0] < 2:
+    matrix = np.random.randint(2, size = (typeVNF, redundantVNF))
+    if not matrix.sum(axis = 0).all():
+        continue
+    population = np.append(population, matrix)
+print(population)
 
 # tiempo ejecución
 end = time.time()
