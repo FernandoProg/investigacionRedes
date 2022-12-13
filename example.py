29e1d902603910a96d88358a27cc6a0f957e20ba
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-12-13 18:39:55 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,14 +1,54 @@
-from itertools import combinations
+from itertools import chain, combinations
 import numpy as np
+from functools import reduce
 
-list = [0.95, 0.99, 0.97]
+def powerset(list_name):
+    s = list(list_name)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-a = (1-(1-list[0])*(1-list[1])*(1-list[2]))*(1-(1-list[0])*(1-list[1])*(1-list[2]))
 
+confmin = 0.95
+conf = [0.95, 0.99, 0.97]
+delays = [0.8, 3, 2]
+
+a = (1-(1-conf[0])*(1-conf[1])*(1-conf[2]))*(1-(1-conf[0])*(1-conf[1])*(1-conf[2]))
 print(a)
-combs = []
-for i in range(len(list)):
-    els = [list(x) for x in combinations(list, i) if len(x) < len(list) ]
-    combs.extend(els)
 
+print(conf)
+numbs = [0, 1, 2]
+combs = []
+
+for x in powerset(numbs):
+    if (x) :
+        combs.append(list(np.array(x)))
+    
+    
 print(combs)
+baseDelay = 10000
+best =[[],[]]
+for i in combs:
+    for j in combs:
+        delay = 0
+        aux = 1
+        for k in i:
+            aux *= (1-conf[k])
+            delay+=delays[k]
+        x = 1-aux
+
+        aux = 1
+        for k in j:
+            aux *= (1-conf[k])
+            delay+=delays[k]
+        y = 1-aux
+       
+        if(x*y>confmin):
+            
+            if (baseDelay>=delay) :
+                print("conf: ",x*y," delay: ", delay, " vfn 1:",i," vnf 2: ",j)
+                baseDelay = delay
+                best[0] = i
+                best[1] = j
+print("minimo delay :", baseDelay, " vnfs ", best)
+
+       
+            
